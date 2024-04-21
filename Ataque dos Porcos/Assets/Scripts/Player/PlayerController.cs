da1649632564aa7c9a96ca011f482f9f56f31110
marcusluccas <lucilenebarbosamoura@gmail.com>
2024-04-21 19:09:48 -0300

    Fazer o player ir no parado por correndo

--- Ataque dos Porcos/Assets/Scripts/Player/PlayerController.cs
+++ Ataque dos Porcos/Assets/Scripts/Player/PlayerController.cs
@@ -6,20 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D myRB;
     private float velocity = 4;
+    private Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal") * velocity;
+        myAnimator.SetBool("Move", false);
         if (horizontal != 0)
         {
             transform.localScale = new Vector3(Mathf.Sign(horizontal), 1f, 1f);
+            myAnimator.SetBool("Move", true);
         }
         myRB.velocity = new Vector2(horizontal, myRB.velocity.y);
     }
