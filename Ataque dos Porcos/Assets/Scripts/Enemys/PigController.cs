2010664f693438f1365ac5322b56a1798192e6cd
marcusluccas <lucilenebarbosamoura@gmail.com>
2024-04-24 23:04:23 -0300

    Fazendo a transição de animação do inimigo

--- Ataque dos Porcos/Assets/Scripts/Enemys/PigController.cs
+++ Ataque dos Porcos/Assets/Scripts/Enemys/PigController.cs
@@ -8,11 +8,13 @@ public class PigController : MonoBehaviour
     private float myVelocity = 2f;
     private float waitMove = 1f;
     private int direction;
+    private Animator myAnimator;
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
         direction = Random.Range(-1, 2);
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,9 +29,11 @@ public class PigController : MonoBehaviour
         if (waitMove <= 0f)
         {
             myRB.velocity = new Vector2(myVelocity * direction, 0f);
+            myAnimator.SetBool("Move", false);
             if (myRB.velocity.x != 0f)
             {
                 transform.localScale = new Vector3(Mathf.Sign(myRB.velocity.x) * -1, 1f, 1f);
+                myAnimator.SetBool("Move", true);
             }
             waitMove = Random.Range(2, 3);
             direction = Random.Range(-1, 2);
